a9256868aac4fb0aa20ac6c25cd9528edd15c579
Aaditya Bhatia <aadityabhatia@gmail.com>
2023-04-02 16:08:26 -0400

    improved test script

--- test-commands.sh
+++ test-commands.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -e
-
-OUTPUT_DIRECTORY=out
+OUTPUT_DIRECTORY=output
 
 printf "Creating output directory: $OUTPUT_DIRECTORY\n"
 mkdir -p $OUTPUT_DIRECTORY
@@ -26,21 +24,35 @@ printf "Loading test cases: $filename.\n"
 
 i=0
 
-printf "Running test cases...\n\n"
-
-function run_test_cases {
+function run_test_case {
 	# read the filename as the first command line argument
 	line="$@"
 	printf "Test case $i.\n$line\n"
-	python sim_cache.py $line >out/$i.txt
-	printf "Output saved to: out/$i.txt\n"
-	test -f assets/output/validation$i.txt &&
-		printf "Comparing output against validation file... " &&
-		{ diff -iw assets/output/validation$i.txt out/$i.txt >out/diff$i.txt; } && printf "Match." || printf "Mismatch. See out/diff$i.txt for details."
-	((i++))
+	python sim_cache.py $line >$OUTPUT_DIRECTORY/$i.txt || exit $?
+	printf "Output saved to: $OUTPUT_DIRECTORY/$i.txt\n"
+
+	# if validation file exists, compare output
+	test -f assets/output/validation$i.txt && diff_output assets/output/validation$i.txt $OUTPUT_DIRECTORY/$i.txt
+	i=$((i + 1))
+}
+
+function diff_output {
+	printf "Comparing output against validation file... "
+	diff_file=$OUTPUT_DIRECTORY/diff$i.txt
+	diff -iwy --suppress-common-lines $1 $2 >$diff_file
+	if [ $? -eq 0 ]; then
+		printf "Match."
+	elif [ $? -eq 1 ]; then
+		printf "Mismatch. See $diff_file"
+	else
+		printf "Error."
+		exit $?
+	fi
+	printf "\n\n"
+	return 0
 }
 
 while read line; do
-	run_test_cases $line || true
-	printf "\n\n"
+	run_test_case $line
+
 done <$filename
